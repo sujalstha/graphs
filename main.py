a876bb5fef5c3051011321b8475fd43ae7dd914b
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-06-13 22:01:19 -0500

    Initial commit

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-import numpy
 import matplotlib.pyplot as plt
 import numpy as np
 
 xaxis = np.array([2, 8])
-yaxis = np.array([4, 9])
+yaxis = np.array([2, 8])
+
+plt.plot(xaxis, yaxis)
+plt.show()
+
+xaxis = np.array([2, 12, 3, 9])
+
+plt.plot(xaxis, marker="o", linestyle="--")  # - = single line
+
+plt.show()
+
+x = [2, 3, 7, 29, 8, 5, 13, 11, 22, 33]
+
+y = [4, 7, 55, 43, 2, 4, 11, 22, 33, 44]
+
+plt.scatter(x, y)
+
+plt.show()
